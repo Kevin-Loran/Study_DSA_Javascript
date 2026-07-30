635af8b7296894bf848144399a68c0bf14ff5632
Kevin-Loran <kevinloran.eng@gmail.com>
2026-07-30 20:44:13 -0300

    feat: DSA/Terminando o estudo de listas duplamente encadeadas.

---
+++ data-structures/Lista-encadeada-extremidades-duplas/classe.py
@@ -0,0 +1,37 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+    def mostrar_no(self):
+        print(self.valor)
+
+class ListaEncadeadaExtremidadeDupla:
+    def __init__(self):
+        self.primeiro = None
+        self.ultimo = None
+
+    def __lista_vazia(self):
+        return self.primeiro == None
+
+    def insere_inicio(self, valor):
+        novo = No(valor)
+        if self.__lista_vazia():
+            self.ultimo = novo
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+
+    def mostrar(self):
+        if self.__lista_vazia():
+            print("lista vazia.")
+            return
+        atual = self.primeiro
+        while atual != None:
+            atual.mostrar_no()
+            atual = atual.proximo
+
+lista = ListaEncadeadaExtremidadeDupla()
+lista.insere_inicio(2)
+lista.insere_inicio(3)
+lista.insere_inicio(4)
+lista.mostrar()
---
+++ data-structures/lista-duplamente-encadeadas/classe.py
@@ -0,0 +1,61 @@
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+        self.anterior = None
+
+    def mostrar_no(self):
+        print(self.valor)
+
+class ListaDuplamenteEncadeada:
+    def __init__(self):
+        self.primeiro = None
+        self.ultimo = None
+
+    def __lista_vazia(self):
+        return self.primeiro == None
+
+    def insere_inicio(self, valor):
+        novo = No(valor)
+        if self.__lista_vazia():
+            self.ultimo = novo
+        else:
+            self.primeiro.anterior = novo
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+
+
+    def insere_final(self, valor):
+        novo = No(valor)
+        if self.__lista_vazia():
+            self.ultimo = novo
+        else:
+            self.ultimo.proximo = novo
+            novo.anterior = self.ultimo
+        self.ultimo = novo
+
+
+    def mostrar_frente(self):
+        atual = self.primeiro
+        while atual != None:
+            atual.mostrar_no()
+            atual = atual.proximo
+
+    def mostrar_tras(self):
+        atual = self.ultimo
+        while atual != None:
+            atual.mostrar_no()
+            atual = atual.anterior
+
+
+lista = ListaDuplamenteEncadeada()
+lista.insere_inicio(5)
+lista.insere_inicio(4)
+lista.insere_inicio(3)
+lista.insere_inicio(2)
+lista.insere_inicio(1)
+lista.mostrar_frente()
+print("=" * 30)
+lista.mostrar_tras()
+
+
