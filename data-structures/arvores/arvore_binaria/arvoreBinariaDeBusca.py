17d52a5519b1f407eea10bb60a3c1c97ac09471e
Kevin-Loran <kevinloran.eng@gmail.com>
2026-08-05 16:50:35 -0300

    feat: DSA/Terminando meus estudos em árvores binárias.

--- data-structures/arvores/arvore_binaria/arvoreBinariaDeBusca.py
+++ data-structures/arvores/arvore_binaria/arvoreBinariaDeBusca.py
@@ -7,6 +7,7 @@ class No:
     def mostrar_no(self):
         print(self.valor)
 
+
 class ArvoreBinaria:
     def __init__(self):
         self.raiz = None
@@ -14,7 +15,7 @@ class ArvoreBinaria:
     def inserir(self, valor):
         novo = No(valor)
         if self.raiz is None:
-            self.raiz = No(valor)
+            self.raiz = novo
             return
         else:
             atual = self.raiz
@@ -33,17 +34,116 @@ class ArvoreBinaria:
                 else:
                     return
 
-    
     def simetric_traversal(self, node=None):
         if node is None:
             node = self.raiz
+            if node is None:
+                return
+
         if node.left:
-            print('(', end='')
+            print("(", end="")
             self.simetric_traversal(node.left)
-        print(node.valor, end='')
+        print(node.valor, end="")
         if node.right:
             self.simetric_traversal(node.right)
             print(")", end="")
+
+    def pesquisa(self, valor):
+        atual = self.raiz
+        while atual is not None:
+            if valor == atual.valor:
+                return atual
+            if valor < atual.valor:
+                atual = atual.left
+            else:
+                atual = atual.right
+        return None
+
+    def pre_ordem(self, no):
+        if no is not None:
+            print(no.valor, end=" ")
+            self.pre_ordem(no.left)
+            self.pre_ordem(no.right)
+
+    def ordem(self, no):
+        if no is not None:
+            self.ordem(no.left)
+            print(no.valor, end=" ")
+            self.ordem(no.right)
+
+    def pos_ordem(self, no):
+        if no is not None:
+            self.pos_ordem(no.left)
+            self.pos_ordem(no.right)
+            print(no.valor, end=" ")
+
+    def exclusao(self, valor):
+        if self.raiz is None:
+            print("A árvore está vazia.")
+            return False
+
+        atual = self.raiz
+        pai = self.raiz
+        e_esquerda = True
+
+        while atual.valor != valor:
+            pai = atual
+            if valor < atual.valor:
+                e_esquerda = True
+                atual = atual.left
+            else:
+                e_esquerda = False
+                atual = atual.right
+            if atual is None:
+                return False
+
+        if atual.left is None and atual.right is None:
+            if atual == self.raiz:
+                self.raiz = None
+            elif e_esquerda:
+                pai.left = None
+            else:
+                pai.right = None
+        elif atual.right is None:
+            if atual == self.raiz:
+                self.raiz = atual.left
+            elif e_esquerda:
+                pai.left = atual.left
+            else:
+                pai.right = atual.left
+        elif atual.left is None:
+            if atual == self.raiz:
+                self.raiz = atual.right
+            elif e_esquerda:
+                pai.left = atual.right
+            else:
+                pai.right = atual.right
+        else:
+            sucessor = self.getSucessor(atual)
+
+            if atual == self.raiz:
+                self.raiz = sucessor
+            elif e_esquerda:
+                pai.left = sucessor
+            else:
+                pai.right = sucessor
+
+            sucessor.left = atual.left
+
+        return True
+
+    def getSucessor(self, no):
+        pai_sucessor = no
+        sucessor = no
+        atual = no.right
+        while atual is not None:
+            pai_sucessor = sucessor
+            sucessor = atual
+            atual = atual.left
+        if sucessor != no.right:
+            pai_sucessor.left = sucessor.right
+            sucessor.right = no.right
+        return sucessor
 
 
 arvore = ArvoreBinaria()
@@ -52,9 +152,18 @@ arvore.inserir(14)
 arvore.inserir(9)
 arvore.inserir(10)
 arvore.inserir(24)
+
+print("Simétrico com parênteses:")
 arvore.simetric_traversal()
+print("\n")
 
+print("Em-Ordem (Crescente):")
+arvore.ordem(arvore.raiz)
+print("\n")
 
+print("Pré-Ordem:")
+arvore.pre_ordem(arvore.raiz)
+print("\n")
 
-
-
+print("Pós-Ordem:")
+arvore.pos_ordem(arvore.raiz)
